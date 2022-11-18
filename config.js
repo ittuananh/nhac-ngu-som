e693bc551e6c41dafef73fee6e736c84b4cd7d48
ittuananh <118551420+ittuananh@users.noreply.github.com>
2022-11-18 23:35:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    Mà sao Bi cứ đi nhầm đường
+    Khom chịu ngủ sớm ii -.-
+    Khom ngủ sớm là bị ông ba bị bắt ó,
+    btnIntro: 'Ngủ sớm ii',
+    title: 'Bi ngủ muộn là hư đóo',
+    desc: 'Bi ngủ sớm nhoo ',
+    btnYes: 'Cóooo',
+    btnNo: 'Khumm',
+    question:'Trước khi ngủ Bị chúc em ngủ ngonn ii',
+    btnReply: 'Gửi cho TA cutee',
+    reply: 'Tuấn Anh ngủ ngon nhóo',
+    mess: 'Òooo cảm ưn ó',
+    messDesc: 'Tối mai Bi cũng ngủ sớm nhooo.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //ta.pham.969952
 }
